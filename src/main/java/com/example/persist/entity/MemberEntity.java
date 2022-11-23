a36fe0563dfbe4be5b0e75760f2973213b8c97f4
정일 <imbolic0301@gmail.com>
2022-11-23 19:45:41 +0900

    feat : 회원 entity 추가

---
+++ src/main/java/com/example/persist/entity/MemberEntity.java
@@ -0,0 +1,10 @@
+package com.example.persist.entity;
+
+
+public class MemberEntity {
+    private Long id;
+    private String phone;
+    private String identifierKor;
+    private String age;
+    private String password;
+}
