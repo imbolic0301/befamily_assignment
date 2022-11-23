3569fe52ed2bb4b9aadf010a57903a48fca69d5a
정일 <imbolic0301@gmail.com>
2022-11-23 20:01:55 +0900

    impl : Member 엔티티에 기본 메소드 추가

--- src/main/java/com/example/persist/entity/MemberEntity.java
+++ src/main/java/com/example/persist/entity/MemberEntity.java
@@ -1,10 +1,60 @@
 package com.example.persist.entity;
 
 
+import java.util.Objects;
+
 public class MemberEntity {
     private Long id;
+    private String email;
     private String phone;
     private String identifierKor;
     private String age;
     private String password;
+
+    public MemberEntity(String phone, String email, String identifierKor, String age, String password) {
+        this.phone = phone;
+        this.email = email;
+        this.identifierKor = identifierKor;
+        this.age = age;
+        this.password = password;
+    }
+
+    public Long id() {
+        return id;
+    }
+
+    public String email() {
+        return email;
+    }
+
+    public String phone() {
+        return phone;
+    }
+
+    public String identifierKor() {
+        return identifierKor;
+    }
+
+    public String age() {
+        return age;
+    }
+
+    public String password() {
+        return password;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MemberEntity)) return false;
+        MemberEntity that = (MemberEntity) o;
+        return id().equals(that.id());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id());
+    }
+
 }
